b09cb3e77b21822d6f12a30065fcd05260d68d78
Tavis <tavis@tav.is>
2022-04-19 14:13:42 -0700

    WIP

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { graphql } = require("@octokit/graphql");
-const isBefore = require('date-fns/isBefore')
-const sub = require('date-fns/sub')
+const { graphql } = require('@octokit/graphql');
+const isBefore = require('date-fns/isBefore');
+const sub = require('date-fns/sub');
 
 const fetchCardsQuery = `query projectCards($owner: String!, $repo: String!, $projectName: String!, $cursor: String!) {
                            repository(owner: $owner, name: $repo) {
@@ -33,7 +33,7 @@ const fetchCardsQuery = `query projectCards($owner: String!, $repo: String!, $pr
                                }
                              }
                            }
-                         }`
+                         }`;
 
 const archiveCardQuery = `mutation archiveCards($cardId: String!, $isArchived: Boolean = true) {
                              updateProjectCard(input:{projectCardId: $cardId, isArchived: $isArchived}) {
@@ -41,9 +41,15 @@ const archiveCardQuery = `mutation archiveCards($cardId: String!, $isArchived: B
                                  id
                                }
                              }
-                           }`
+                           }`;
 
-async function fetchCards(repoOwner, repo, projectName, currentCursor, accessToken) {
+async function fetchCards(
+  repoOwner,
+  repo,
+  projectName,
+  currentCursor,
+  accessToken
+) {
   return graphql(fetchCardsQuery, {
     owner: repoOwner,
     repo: repo,
@@ -51,85 +57,107 @@ async function fetchCards(repoOwner, repo, projectName, currentCursor, accessTok
     cursor: currentCursor,
     headers: {
       authorization: `bearer ${accessToken}`,
-    }
-  })
+    },
+  });
 }
-
 
 const dateifyCard = (card) => {
-  const updatedAt = new Date(card.updatedAt)
-  return { id: card.id, updatedAt: updatedAt }
-}
+  const updatedAt = new Date(card.updatedAt);
+  return { id: card.id, updatedAt: updatedAt };
+};
 
-async function fetchCardInfo(repoOwner, repo, projectName, accessToken, columnToArchive) {
+async function fetchCardInfo(
+  repoOwner,
+  repo,
+  projectName,
+  accessToken,
+  columnToArchive
+) {
   try {
-    const projectCardIdsWithDate = []
-    let currentCursor = ''
-    let nextPage = true
+    const projectCardIdsWithDate = [];
+    let currentCursor = '';
+    let nextPage = true;
 
-    while(nextPage) {
-      let projectCards = await fetchCards(repoOwner, repo, projectName, currentCursor, accessToken)
-      projectCards = projectCards.repository.projects.edges[0].node.columns.edges.find(edge => edge.node.name.toLowerCase() === columnToArchive.toLowerCase()).node.cards
-      projectCardIdsWithDate.push(...projectCards.edges.flatMap(card => card.node).map(dateifyCard))
+    while (nextPage) {
+      let projectCards = await fetchCards(
+        repoOwner,
+        repo,
+        projectName,
+        currentCursor,
+        accessToken
+      );
+      console.log(projectCards);
+      projectCards =
+        projectCards.repository.projects.edges[0].node.columns.edges.find(
+          (edge) =>
+            edge.node.name.toLowerCase() === columnToArchive.toLowerCase()
+        ).node.cards;
+      projectCardIdsWithDate.push(
+        ...projectCards.edges.flatMap((card) => card.node).map(dateifyCard)
+      );
 
-      currentCursor = projectCards.pageInfo.endCursor
-      nextPage = projectCards.pageInfo.hasNextPage
+      currentCursor = projectCards.pageInfo.endCursor;
+      nextPage = projectCards.pageInfo.hasNextPage;
     }
 
-    return projectCardIdsWithDate
-  }
-  catch(e) {
-    console.log('fetchCardInfo error: ', e)
-    return []
+    return projectCardIdsWithDate;
+  } catch (e) {
+    console.log('fetchCardInfo error: ', e);
+    return [];
   }
 }
 
 const run = async () => {
   try {
-    const accessToken = core.getInput('access-token')
-    const columnToArchive = core.getInput('column-to-archive')
-    const repoOwner = core.getInput('repository-owner')
-    const repo = core.getInput('repository')
-    const projectName = core.getInput('project-name')
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const accessToken = core.getInput('access-token');
+    const columnToArchive = core.getInput('column-to-archive');
+    const repoOwner = core.getInput('repository-owner');
+    const repo = core.getInput('repository');
+    const projectName = core.getInput('project-name');
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
 
     const daysOld = core.getInput('days-old');
-    const cutoffDate = sub(new Date(), { days: daysOld })
+    const cutoffDate = sub(new Date(), { days: daysOld });
 
-    console.log(`Archiving all cards that have been untouched for ${daysOld} days from column ${columnToArchive}!`);
+    console.log(
+      `Archiving all cards that have been untouched for ${daysOld} days from column ${columnToArchive}!`
+    );
 
     console.log(`The event payload: ${payload}`);
 
-    const projectCardIdsWithDate = await fetchCardInfo(repoOwner, repo, projectName, accessToken, columnToArchive)
+    const projectCardIdsWithDate = await fetchCardInfo(
+      repoOwner,
+      repo,
+      projectName,
+      accessToken,
+      columnToArchive
+    );
 
     // Filter by updated at date
     const cardIdsToArchive = projectCardIdsWithDate
-          .filter(card => isBefore(card.updatedAt, cutoffDate))
-          .map(node => node.id)
+      .filter((card) => isBefore(card.updatedAt, cutoffDate))
+      .map((node) => node.id);
 
     // Archive those - https://docs.github.com/en/free-pro-team@latest/rest/reference/projects#update-an-existing-project-card
 
-    console.log(`Archiving ${cardIdsToArchive.length} cards`)
+    console.log(`Archiving ${cardIdsToArchive.length} cards`);
 
     cardIdsToArchive.forEach(async (id) => {
       try {
         await graphql(archiveCardQuery, {
-                cardId: id,
-                headers: {
-                  authorization: `bearer ${accessToken}`,
-                },
-        })
+          cardId: id,
+          headers: {
+            authorization: `bearer ${accessToken}`,
+          },
+        });
+      } catch (e) {
+        console.log('archiveCard error: ', e);
+        return false;
       }
-      catch(e) {
-        console.log('archiveCard error: ', e)
-        return false
-      }
-    })
-
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
+};
 
-}
-
-run()
+run();
